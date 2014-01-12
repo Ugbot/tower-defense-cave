c465a956764338169a48bdf23ff2b66c49c32804
Abner Coimbre <abner.coimbre@upr.edu>
2014-01-12 13:53:47 -0500

    refactored TowerControl class

--- src/com/cavedwellers/controls/TowerControl.java
+++ src/com/cavedwellers/controls/TowerControl.java
@@ -10,8 +10,6 @@ import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.control.AbstractControl;
 import com.jme3.scene.shape.Line;
-import java.util.ArrayList;
-import java.util.List;
 import com.cavedwellers.states.GameRunningState;
 
 /**
@@ -25,14 +23,11 @@ public class TowerControl extends AbstractControl
 {
     private GameRunningState currentGameState;
     private Node beamNode;
-    private List<Spatial> enemies = null;
-    private ArrayList<EnemyControl> reachable;
-
+    
     public TowerControl(GameRunningState state) 
     {
         currentGameState = state;
         beamNode = currentGameState.getNode("beam");
-        reachable = new ArrayList<>();
     }
 
     @Override
@@ -41,45 +36,21 @@ public class TowerControl extends AbstractControl
         if (currentGameState.isPaused() || currentGameState.isPlayerAddingTower())
             return;
 
-        enemies = currentGameState.getNode("enemy").getChildren();
-
-        /* Check if enemies are in range */
-        for (int i = 0; i < enemies.size(); i++) 
-        {
-            if (enemies.get(i).getLocalTranslation().getZ() - spatial.getLocalTranslation().getZ() < 10 
-                || enemies.get(i).getLocalTranslation().getX() - spatial.getLocalTranslation().getX() < 10) 
-            {
-                reachable.add(enemies.get(i).getControl(EnemyControl.class));
-            }
-        }
-
-        /* Shoot beams at the enemies in range */
-        if (reachable.size() > 0) 
-        {
-            /* Attack each enemy in range */
-            for (int i = 0; i < reachable.size(); i++) 
-            {
-                // FIXME: Ugly hack. First thing on the list to fix
-                if (spatial.getUserData("type").equals("laser") && reachable.get(i).getSpatial().getName().startsWith("ghost")) 
-                    continue;
-                
-                useBeam(reachable.get(i));
-            }
-        }
-        reachable.clear();
+        for (Spatial enemy : currentGameState.getNode("enemy").getChildren())
+            if (enemy.getLocalTranslation().getZ() - spatial.getLocalTranslation().getZ() < 10 || enemy.getLocalTranslation().getX() - spatial.getLocalTranslation().getX() < 10)
+                attackEnemy(enemy);
     }
 
-    @Override
-    public void controlRender(RenderManager rm, ViewPort vp) 
-    {}
+    public void attackEnemy(Spatial enemy) 
+    {
+        if (spatial.getUserData("type").equals("laser") && enemy.getName().startsWith("ghost")) 
+            return;
+        
+        Vector3f beamStartLocation = new Vector3f(spatial.getLocalTranslation().getX(), getTowerHeight(), spatial.getLocalTranslation().getZ());
+        beamNode.attachChild(getBeam(beamStartLocation, enemy.getLocalTranslation()));
+        enemy.getControl(EnemyControl.class).decreaseHealth((int)spatial.getUserData("damage"));
+    }
 
-    /**
-     * Creates a beam from a start location to an end location. Needs to be
-     * attached to the beamNode in order to be visible.
-     * @param start a 3D vector specifying the beam's start location
-     * @param end a 3D vector specifying the beam's end location
-     * @return the beam
-     */
     public Geometry getBeam(Vector3f start, Vector3f end) 
     {
         Line lineMesh = new Line(start, end);
@@ -96,27 +67,12 @@ public class TowerControl extends AbstractControl
         return line;
     }
 
-    /**
-     * Represents a tower's beam attack. It makes a beam geometry visible
-     * by attaching it to beamNode and decreases the targeted
-     * enemy's health.
-     * @param enemy the enemy to be attacked
-     */
-    public void useBeam(EnemyControl enemy) 
-    {
-        float towerX = spatial.getLocalTranslation().getX();
-        float towerZ = spatial.getLocalTranslation().getZ();
-        Vector3f beamStart = new Vector3f(towerX, getHeight(), towerZ);
-        beamNode.attachChild(getBeam(beamStart, enemy.getSpatial().getLocalTranslation()));
-        enemy.decreaseHealth((int)spatial.getUserData("damage"));
-    }
-
-    /**
-     * Gets the tower's height.
-     * @return the float indicating the tower's height
-     */
-    public float getHeight() 
+    public float getTowerHeight() 
     {
         return 16f;
     }
+    
+    @Override
+    public void controlRender(RenderManager rm, ViewPort vp) 
+    {}
 }
